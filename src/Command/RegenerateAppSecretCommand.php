d8f2d17cda8e617281d07d76318df2fac8b4775a
Tudgwal <gregoire.dauvin@padam.io>
2024-05-03 19:07:44 +0200

    readme

---
+++ src/Command/RegenerateAppSecretCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'regenerate-app-secret',
+    description: 'Regenerate APP_SECRET in your .env.local',
+)]
+class RegenerateAppSecretCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $a = '0123456789abcdef';
+        $secret = '';
+        for ($i = 0; $i < 32; $i++) {
+            $secret .= $a[rand(0, 15)];
+        }
+
+        $r = shell_exec('sed -i -E "s/^APP_SECRET=/APP_SECRET=' . $secret . '/" .env.local');
+
+        $io->success('New APP_SECRET was generated: ' . $secret);
+
+        return 0;
+    }
+
+}
